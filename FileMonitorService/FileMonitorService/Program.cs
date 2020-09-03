3b693d5ab8ff3dd6d0d47365b52fc482f2412db5
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-03 19:57:30 +0300

    Made the code run as a service on the computer

--- FileMonitorService/FileMonitorService/Program.cs
+++ FileMonitorService/FileMonitorService/Program.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using System;
 
 namespace FileMonitorService
 {
@@ -40,6 +37,7 @@ namespace FileMonitorService
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
